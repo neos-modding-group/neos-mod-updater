b8462327d9c9ba0d30d99a5db6b49843170b1f26
Michael Ripley <zkxs00@gmail.com>
2022-03-20 22:37:18 -0500

    sha256 vs sha512 peformance test
    Results:
    At first there's some filesystem caching garbage polluting the results, so we do some fake warmup runs to get the OS used to our workload.
    
    Once we're no longer IO bound, we max out a thread and see sha256 go about 2.25 times faster than sha512.
    
    So, crypto-wise sha256 is plenty strong, the hash is shorter, and it runs faster. We found our winner.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,81 @@
+use std::{fs, io};
 use std::path::Path;
-use pelite::FileMap;
+use std::time::Instant;
+
 use pelite::pe32::{Pe, PeFile};
+use sha2::{Digest, Sha256, Sha512};
 
 fn main() {
+    hash_test();
+}
+
+fn hash_test() {
+
+    let path = r"C:\Program Files (x86)\Steam\steamapps\common\NeosVR\Libraries\NeosModLoader.dll";
+    let mut buf = [0u8; 128];
+
+    {
+        println!("warmup 1...");
+        let mut file = fs::File::open(path).unwrap();
+        let mut hasher = Sha512::new();
+        let now = Instant::now();
+        let n = io::copy(&mut file, &mut hasher).unwrap();
+        let output = hasher.finalize();
+        let elapsed = now.elapsed().as_secs_f64();
+        let hash = base16ct::lower::encode_str(&output, &mut buf).unwrap();
+        println!("in {}s hashed {} bytes of {} to get {:?}", elapsed, n, path, hash);
+    }
+
+    {
+        println!("warmup 2...");
+        let mut file = fs::File::open(path).unwrap();
+        let mut hasher = Sha256::new();
+        let now = Instant::now();
+        let n = io::copy(&mut file, &mut hasher).unwrap();
+        let output = hasher.finalize();
+        let elapsed = now.elapsed().as_secs_f64();
+        let hash = base16ct::lower::encode_str(&output, &mut buf).unwrap();
+        println!("in {}s hashed {} bytes of {} to get {:?}", elapsed, n, path, hash);
+    }
+
+    {
+        println!("warmup 3...");
+        let mut file = fs::File::open(path).unwrap();
+        let mut hasher = Sha512::new();
+        let now = Instant::now();
+        let n = io::copy(&mut file, &mut hasher).unwrap();
+        let output = hasher.finalize();
+        let elapsed = now.elapsed().as_secs_f64();
+        let hash = base16ct::lower::encode_str(&output, &mut buf).unwrap();
+        println!("in {}s hashed {} bytes of {} to get {:?}", elapsed, n, path, hash);
+    }
+
+    {
+        println!("sha 256 test...");
+        let mut file = fs::File::open(path).unwrap();
+        let mut hasher = Sha256::new();
+        let now = Instant::now();
+        let n = io::copy(&mut file, &mut hasher).unwrap();
+        let output = hasher.finalize();
+        let elapsed = now.elapsed().as_secs_f64();
+        let hash = base16ct::lower::encode_str(&output, &mut buf).unwrap();
+        println!("in {}s hashed {} bytes of {} to get {:?}", elapsed, n, path, hash);
+    }
+
+    {
+        println!("sha 512 test...");
+        let mut file = fs::File::open(path).unwrap();
+        let mut hasher = Sha512::new();
+        let now = Instant::now();
+        let n = io::copy(&mut file, &mut hasher).unwrap();
+        let output = hasher.finalize();
+        let elapsed = now.elapsed().as_secs_f64();
+        let hash = base16ct::lower::encode_str(&output, &mut buf).unwrap();
+        println!("in {}s hashed {} bytes of {} to get {}", elapsed, n, path, hash);
+    }
+}
+
+fn version_test() {
     // this works
     println!("NML: {:?}", get_version(r"C:\Program Files (x86)\Steam\steamapps\common\NeosVR\Libraries\NeosModLoader.dll"));
 
@@ -11,7 +84,7 @@ fn main() {
 }
 
 fn get_version<P: AsRef<Path> + ?Sized>(path: &P) -> String {
-    let map = FileMap::open(path).unwrap();
+    let map = pelite::FileMap::open(path).unwrap();
     let pe = PeFile::from_bytes(&map).unwrap();
     let res = pe.resources().unwrap();
     let v = res.version_info().unwrap();
